9986e6a8d284379078d91784049670f65bff79ec
Ben Yee <l3enny@gmail.com>
2013-07-15 17:27:52 -0400

    Theory revisions, work on streamer sim, additional data files.

--- simulations/Streamer Criteria/distance.py
+++ simulations/Streamer Criteria/distance.py
@@ -1,26 +1,61 @@
+from math import log, exp
 import numpy as np
 from scipy.constants import *
-from scipy.interpolate import UnivariateSpline
+from scipy.interpolate import UnivariateSpline as Spline
+from scipy.integrate import quad, trapz
+from scipy.special import erf
+import matplotlib.pyplot as plt
 
-P = 1.0
-Tg = 300
+# User settings
+P    = 1.0           # pressure, Torr
+T_g  = 300           # gas temperature, K
+dENdt = 1e-21 / 1e-09 # rate of rise of electric field, V-m^2/s
+EN_m = 100e-21       # maximum applied electric field, V-m^2
+eps  = 1e-20         # solution convergence criteria
+h    = 1e-12         # integrator time step
 
-Ng = lambda P: 133.332 * P / (k * Tg)
+# Values @ breakdown in equilibrium
+EN_0 = 4.4e-21    # Breakdown reduced electric field, V-m^2
+lambda_e = 3.34e-19   # mean momentum transfer cross section, m^2
 
-mu_data = np.loadtxt('../BOLSIG+/mobility.dat', delimiter='\t', skiprows=1)
-E_sim = mu_data[:, 0] * 1e-21 * Ng
-mu = UnivariateSpline(E_sim, mu_data[:, 1], s=0)
-alpha_data = np.loadtxt('../BOLSIG+/townsend_coeff.dat', delimiter='\t', skiprows=1)
-alpha = UnivariateSpline(E_sim, alpha_data[:, 1], s=0)
+# Derived quantities
+t0  = EN_0 / dENdt
+N_g  = 133.332 * P / (k * T_g) # gas density
 
-ENg = data[:, 0]
-def E(t):
-    slope = 1e3
-    tm = 25e-9
-    if t > tm:
-        return 0.0
-    else:
-        return slope * t
+# Load BOLSIG+ simulation data
+muN_data = np.loadtxt('../BOLSIG+/mobility.dat', delimiter='\t', skiprows=1)
+alphaN_data = np.loadtxt('../BOLSIG+/townsend_coeff.dat', delimiter='\t', skiprows=1)
+energy_data = np.loadtxt('../BOLSIG+/mean_energy.dat', delimiter='\t',
+        skiprows=1)
 
-E_s = []
+energy_spline = Spline(1e-21 * energy_data[:, 0], energy_data[:, 1], s=0)
+muN_spline = Spline(1e-21 * muN_data[:, 0], muN_data[:, 1], s=0)
+alpha_spline = Spline(1e-21 * alphaN_data[:, 0], alphaN_data[:, 1] * N_g, s=0)
+
+def EN(t):
+    return min(dENdt * t, EN_m)
+
+def C_e(EN):
+    energy = energy_spline(EN)
+    if energy < 0:
+        energy = 300 * k
+    return (2./3) * (2. * energy * e / m_e)**0.5
+
 t = 0.0
+u_e = [0.0]
+E_r = [0.0]
+xi  = [0.0]
+alpha = [0.0]
+while E_r[-1] <= EN(t):
+    u_e.append(u_e[-1] + muN_spline(EN(t)) * dENdt * h)
+    if t > t0:
+        xi.append(xi[-1] + muN_spline(EN(t)) * dENdt * h)
+        alpha.append(alpha_spline(EN(t)))
+        E_r.append(0.0511 * e * np.mean(u_e) * exp(trapz(alpha, xi))
+                   / (epsilon_0 * lambda_e * C_e(EN(t)) * xi[-1]))
+    else:
+        E_r.append(0.0)
+        alpha.append(0.0)
+        xi.append(0.0)
+    t += h
+print E_r
