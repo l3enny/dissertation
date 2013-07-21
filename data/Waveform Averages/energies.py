e05b80af556810cf11fbbed0b2bb6eff9f944082
Ben Yee <l3enny@gmail.com>
2013-07-21 14:29:59 -0400

    Energy results, plots.

--- data/Waveform Averages/energies.py
+++ data/Waveform Averages/energies.py
@@ -7,6 +7,7 @@ import os
 starts = np.linspace(167e-9, 187e-9)
 reflect = 138e-9
 absorbed = []
+debug = False
 
 t = np.linspace(0,2e3 * 1e-9, 5e3)
 dt = (2e3 * 1e-9) / 5e3
@@ -16,6 +17,9 @@ dirs = os.walk('.')
 for d in dirs:
     if 'current.csv' in d[2]:
         print "Maximizing absorption\n"
+        current = np.loadtxt(d[0] + '\\current.csv', delimiter=',')
+        voltage = np.loadtxt(d[0] + '\\voltage.csv', delimiter=',')
+        absorbed = []
         for start in starts:
             i = round(start/dt, 0)
             j = round((start + reflect)/dt, 0)
@@ -31,17 +35,20 @@ for d in dirs:
             #print "Reflected (J) =", reflected
             #print "Absorbed (J) =", absorbed
 
-plt.plot(starts, absorbed)
-plt.show()
 
-#index = np.argmax(absorbed)
-index = 25
-print "Start (s) =", starts[index]
-print "Absorbed (mJ) =", (absorbed[index] * 1e3)
+        #index = np.argmax(absorbed)
+        index = 25
+        #print "Start (s) =", starts[index]
+        print "Pressure =", d[0][2:]
+        print "Absorbed (mJ) =", (absorbed[index] * 1e3)
+        print "Uncertainty (mJ) =", ((absorbed[index] - absorbed[index-1])* 1e3)
 
-plt.hold(True)
-plt.plot(t, voltage)
-plt.plot(t, current)
-plt.vlines((starts[25], starts[25]+reflect, starts[25]+2*reflect), -15, 15)
-plt.axis((starts[25], starts[25] + 2*reflect, -15, 15))
-plt.show()
+        if debug:
+            plt.plot(starts, absorbed)
+            plt.show()
+            plt.hold(True)
+            plt.plot(t, voltage / max(abs(voltage)))
+            plt.plot(t, current / max(abs(current)))
+            plt.vlines((starts[25], starts[25]+reflect, starts[25]+2*reflect), -15, 15)
+            plt.axis((starts[25], starts[25] + 2*reflect, -1, 1))
+            plt.show()
