8f69b0d8be96d9e21775d5095ab6d840402db4aa
Ben Yee <l3enny@gmail.com>
2013-09-11 11:05:43 -0400

    Fixed BOLSIG+ cross-section generation script.

--- simulations/XPDP1 Xsections/xsections.py
+++ simulations/XPDP1 Xsections/xsections.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
 extengy0 = 19.8
 ionenergy0 = 24.5
@@ -40,7 +41,7 @@ def hsigma3(energies):
                       / ((energies[i] + 50) * (energies[i] + 300)**1.2)
     return output
 
-resolution = 1e2
+resolution = 1e3
 energies = np.logspace(-3, 3, num=resolution)
 
 elastic = np.zeros(resolution)
@@ -53,41 +54,65 @@ momentum = np.zeros(resolution)
 momentum = elastic + excitation + ionization
 
 fn = "elastic.dat"
+header = "Energy (eV),Cross Section (m^2)\r\n"
+date = datetime.datetime.now()
 with open(fn, mode="wb") as f:
-    f.write("Energy (eV),Cross Section (m^2)\n")
+    f.write(header)
     formatted = np.array((energies, elastic)).T
-    np.savetxt(fn, formatted, delimiter=",")
+    np.savetxt(f, formatted, delimiter=",")
 
 fn = "excitation.dat"
 with open(fn, mode="wb") as f:
-    f.write("Energy (eV),Cross Section (m^2)\n")
+    f.write(header)
     formatted = np.array((energies, excitation)).T
-    np.savetxt(fn, formatted, delimiter=",")
+    np.savetxt(f, formatted, delimiter=",")
 
 fn = "ionization.dat"
 with open(fn, mode="wb") as f:
-    f.write("Energy (eV),Cross Section (m^2)\n")
+    f.write(header)
     formatted = np.array((energies, ionization)).T
-    np.savetxt(fn, formatted, delimiter=",")
+    np.savetxt(f, formatted, delimiter=",")
 
 filename = "bolsig_conv.txt"
 with open(filename, mode="wb") as f:
-    f.write("ELASTIC\nHe\n0.00013693 / mass ratio\n")
-    f.write("COMMENT: elastic scattering, from XPDP1\n")
-    f.write("----------------------------------------\n")
-    for i in range(len(energies)):
-        f.write(" %e\t%e\n" % (energies[i], elastic[i]))
-    f.write("\n")
+    f.write("These are the cross sections for helium that are used in\r\n"
+            "XPDP1 as developed by Verboncoeur et al. The formatting of\r\n"
+            "this file will hopefully work.\r\n\r\n")
+    f.write("----------------------------------------\r\n\r\n")
+    f.write("COMMENT\r\nHe\r\n")
+    f.write("References:\r\n")
+    f.write("1) Verboncoeur et al., J. Comp. Physics, 104, pp. 321-328 (1993).\r\n")
+    f.write("http://ptsg.egr.msu.edu/pub/codes/xpdp1/\r\n")
+    f.write("-----------------------------------------"
+            "-----------------------------------------\r\n\r\n")
 
-    f.write("EXCITATION\nHe -> He*(19.8eV)\n19.8\n")
-    f.write("COMMENT: excitation, from XPDP1\n")
-    f.write("----------------------------------------\n")
+    f.write("ELASTIC\r\nHe\r\n1.3693e-4\r\n")
+    f.write("COMMENT: elastic scattering, from XPDP1\r\n")
+    f.write("UPDATED: %s\r\n" % date)
+    f.write("-----------------------------------------\r\n")
     for i in range(len(energies)):
-        f.write(" %e\t%e\n" % (energies[i], excitation[i]))
-    f.write("\n")
+        f.write(" %e\t%e\r\n" % (energies[i], elastic[i]))
+    f.write("-----------------------------------------\r\n\r\n")
 
-    f.write("IONIZATION\nHe -> He^+\n24.5\n")
-    f.write("COMMENT: ionization, from XPDP1\n")
-    f.write("----------------------------------------\n")
+    f.write("EXCITATION\r\nHe -> He*\r\n19.8\r\n")
+    f.write("COMMENT: excitation, from XPDP1\r\n")
+    f.write("UPDATED: %s\r\n" % date)
+    f.write("-----------------------------------------\r\n")
     for i in range(len(energies)):
-        f.write(" %e\t%e\n" % (energies[i], ionization[i]))
+        if excitation[i] == 0.0:
+            continue
+        f.write(" %e\t%e\r\n" % (energies[i], excitation[i]))
+    f.write("-----------------------------------------\r\n\r\n")
+
+    f.write("IONIZATION\r\nHe -> He^+\r\n24.5\r\n")
+    f.write("COMMENT: ionization, from XPDP1\r\n")
+    f.write("UPDATED: %s\r\n" % date)
+    f.write("-----------------------------------------\r\n")
+    for i in range(len(energies)):
+        if ionization[i] == 0.0:
+            continue
+        f.write(" %e\t%e\r\n" % (energies[i], ionization[i]))
+    f.write("-----------------------------------------\r\n\r\n\r\n\r\n")
+    f.write("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\r\n")
+    f.write("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\r\n")
+    f.write("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\r\n")
